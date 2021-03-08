63749643d7dce9cbff76cdad9d52b56426f7b5fa
esthermliu <esther.en.liu@gmail.com>
2021-03-07 18:38:09 -0800

    forms update

--- app/forms.py
+++ app/forms.py
@@ -19,7 +19,17 @@ class RegistrationForm(FlaskForm):
     gender = SelectField('Gender', choices=[(0, 'Male'), (1, 'Female'), (2, 'Other')], coerce=int)
     address = StringField('Address', validators=[DataRequired()])
     city = StringField('City', validators=[DataRequired()])
-    state = SelectField('State', choices=[(0, 'Alabama'), (1, 'Alaska')], coerce=int)
+    state = SelectField('State', choices=[(0, 'Alabama'), (1, 'Alaska'), (2, 'Arizona'), (3, 'Arkansas'), (4, 'California'),
+                                            (5, 'Colorado'), (6, 'Connecticut'), (7, 'Delaware'), (8, 'Florida'), (9, 'Georgia'),
+                                            (10, 'Hawaii'), (11, 'Idaho'), (12, 'Illinois'), (13, 'Indiana'), (14, 'Iowa'),
+                                            (15, 'Kansas'), (16, 'Kentucky'), (17, 'Louisiana'), (18, 'Maine'), (19, 'Maryland'),
+                                            (20, 'Massachusetts'), (21, 'Michigan'), (22, 'Minnesota'), (23, 'Mississippi'),
+                                            (24, 'Missouri'), (25, 'Montana'), (26, 'Nebraska'), (27, 'Nevada'), (28, 'New Hampshire'),
+                                            (29, 'New Jersey'), (30, 'New Mexico'), (31, 'New York'), (32, 'North Carolina'), (33, 'North Dakota'),
+                                            (34, 'Ohio'), (35, 'Oklahoma'), (36, 'Oregon'), (37, 'Pennsylvania'), (38, 'Rhode Island'),
+                                            (39, 'South Carolina'), (40, 'South Dakota'), (41, 'Tennessee'), (42, 'Texas'), (43, 'Utah'),
+                                            (44, 'Vermont'), (45, 'Virginia'), (46, 'Washington'), (47, 'West Virginia'), (48, 'Wisconsin'),
+                                            (49, 'Wyoming')], coerce=int)
     zip_code = IntegerField('ZIP', validators=[DataRequired()])
     datepicker = DateField('Birthday', format='%m/%d/%Y', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
